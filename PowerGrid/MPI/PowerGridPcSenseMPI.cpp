b2a90cd8d4e51d810b362f468b97359831f61ba2
Alex Cerjanic <acerja2@illinois.edu>
2018-02-27 19:54:42 -0600

    Updating PowerGridPcSenseMPI for Blue Waters.

--- PowerGrid/MPI/PowerGridPcSenseMPI.cpp
+++ PowerGrid/MPI/PowerGridPcSenseMPI.cpp
@@ -34,6 +34,7 @@ Developed by:
 #include "ismrmrd/version.h"
 #include "ismrmrd/xml.h"
 #include "../processIsmrmrd.hpp"
+#include "../processNIFTI.hpp"
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
@@ -49,7 +50,7 @@ int main(int argc, char **argv) {
   std::string rawDataFilePath, outputImageFilePath;
   uword Nx, Ny, Nz, NShots = 1, type, L = 0, NIter = 10;
   double beta = 0.0;
-
+  uword dims2penalize = 3;
   po::options_description desc("Allowed options");
   desc.add_options()("help,h", "produce help message")(
       "inputData,i", po::value<std::string>(&rawDataFilePath)->required(),
@@ -72,10 +73,11 @@ int main(int argc, char **argv) {
        ("Nx,x", po::value<uword>(&Nx)->required(), "Image size in X (Required)")
        ("Ny,y", po::value<uword>(&Ny)->required(),
                                      "Image size in Y (Required)")
-          ("Nz,z", po::value<uword>(&Nz)->required(), "Image size in Z (Required)")
-          ("NShots,s", po::value<uword>(&NShots), "Number of shots per image")
-          ("Beta,B", po::value<double>(&beta), "Spatial regularization penalty weight")
-          ("CGIterations,n", po::value<uword>(&NIter), "Number of preconditioned conjugate gradient interations for main "
+      ("Nz,z", po::value<uword>(&Nz)->required(), "Image size in Z (Required)")
+      ("NShots,s", po::value<uword>(&NShots), "Number of shots per image")
+      ("Beta,B", po::value<double>(&beta), "Spatial regularization penalty weight")
+      ("Dims2Penalize,D", po::value<uword>(&dims2penalize), "Dimensions to apply regularization to (2 or 3)")
+      ("CGIterations,n", po::value<uword>(&NIter), "Number of preconditioned conjugate gradient interations for main "
           "solver");
 
   po::variables_map vm;
@@ -111,6 +113,8 @@ int main(int argc, char **argv) {
   arma::Col<float> FM;
   arma::Col<std::complex<float>> sen;
   arma::Col<float> PMap;
+  arma::Col<float> FMap;
+  arma::Col<std::complex<float>> SMap;
   ISMRMRD::Dataset *d;
   ISMRMRD::IsmrmrdHeader hdr;
 	acqTracking* acqTrack;
@@ -130,7 +134,7 @@ int main(int argc, char **argv) {
   d->readAcquisition(0, acq);
   uword nro = acq.number_of_samples();
   uword nc = acq.active_channels();
-  
+
   Col<float> ix, iy, iz;
   initImageSpaceCoords(ix, iy, iz, Nx, Ny, Nz);
   Col<float> kx(nro), ky(nro), kz(nro), tvec(nro);
@@ -170,6 +174,9 @@ int main(int argc, char **argv) {
       std::cout << "About to loop through the counters and scan the file"
               << std::endl;
     }
+    std::string baseFilename = "pcSENSE";
+  	std::string filename;
+
 	uword NSet = 0; //Set is only used for arrayed ADCs
 	uword NSeg = 0;
 	for (uword NPhase = 0; NPhase<=NPhaseMax; NPhase++) {
@@ -177,11 +184,18 @@ int main(int argc, char **argv) {
 			for (uword NAvg = 0; NAvg<=NAvgMax; NAvg++) {
 				for (uword NRep = 0; NRep<NRepMax+1; NRep++) {
 					for (uword NSlice = 0; NSlice<=NSliceMax; NSlice++) {
+            filename = baseFilename + "_" + "Slice" + std::to_string(NSlice) +
+								"_" + "Rep" + std::to_string(NRep) + "_" + "Avg" + std::to_string(NAvg) +
+								"_" + "Echo" + std::to_string(NEcho) + "_" + "Phase" + std::to_string(NPhase);
+
 						getCompleteISMRMRDAcqData<float>(d, acqTrack, NSlice, NRep, NAvg, NEcho, NPhase, data, kx, ky,
 								kz, tvec);
 
 						PMap = getISMRMRDCompletePhaseMap<float>(d, NSlice, NSet, NRep, NAvg, NPhase, NEcho, NSeg,
 								(uword) (Nx*Ny*Nz));
+						SMap = getISMRMRDCompleteSENSEMap<std::complex<float>>(d, NSlice, (uword) (Nx*Ny*Nz));
+						FMap = getISMRMRDCompleteFieldMap<float>(d, NSlice, (uword) (Nx*Ny*Nz));
+
               if (world.rank() == 0) {
                 std::cout << "Number of elements in kx = " << kx.n_rows << std::endl;
                 std::cout << "Number of elements in ky = " << ky.n_rows << std::endl;
@@ -201,7 +215,10 @@ int main(int argc, char **argv) {
 
               ImageTemp = reconSolve<float, mpipcSENSE<float>, QuadPenalty<float>>(data, S_DWI, R, kx, ky, kz, Nx,
                 Ny, Nz, tvec, NIter);
-              writeISMRMRDImageData<float>(d, ImageTemp, Nx, Ny, Nz);
+              //writeISMRMRDImageData<float>(d, ImageTemp, Nx, Ny, Nz);
+              if (world.rank() == 0) {
+                writeNiftiMagPhsImage<float>(filename,ImageTemp,Nx,Ny,Nz);
+              }
             }
           }
         }
